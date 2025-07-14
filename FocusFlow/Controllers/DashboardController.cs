f8a816667b2d24360248be8f90f4c8c795cdeedd
Keagen Talsma <keegenjt@gmail.com>
2025-07-13 21:00:23 -0400

    Update the HomeController to DashboardController

--- FocusFlow/Controllers/DashboardController.cs
+++ FocusFlow/Controllers/DashboardController.cs
@@ -4,11 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FocusFlow.Controllers;
 
-public class HomeController : Controller
+public class DashboardController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<DashboardController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public DashboardController(ILogger<DashboardController> logger)
     {
         _logger = logger;
     }
