f39c75372ef26c37c435a05058de327f8d4719fc
Keagen Talsma <keegenjt@gmail.com>
2025-07-16 15:44:31 -0400

    Convert Task entities to TaskSummaryViews, pass to DashboardView

--- FocusFlow/Controllers/DashboardController.cs
+++ FocusFlow/Controllers/DashboardController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 using FocusFlow.Data;
+using FocusFlow.Models;
 
 namespace FocusFlow.Controllers;
 
@@ -15,6 +16,23 @@ public class DashboardController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        var today = DateOnly.FromDateTime(DateTime.Now);
+        var taskSummaries = this._dbContext.Tasks
+            .Where(task => task.DueDate == today)
+            .Select(task => new TaskSummaryViewModel {
+                Title = task.Title,
+                Description  =  task.Description,
+                IsCompleted  =   task.IsCompleted,
+                TimeLoggedString  = $"{task.SecondsLogged / 60} mins", 
+                DueDateString  = task.DueDate.ToString("ddd, MMM dd"), 
+                IsActive = false
+            }).ToList();
+
+        var dashboardView = new DashboardViewModel
+        {
+            TaskSummaries = taskSummaries
+        };
+            
+        return View(dashboardView);
     }
 }
