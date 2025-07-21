7dd51017c24803d89f3469dade2460ee065849e6
Keagen Talsma <keegenjt@gmail.com>
2025-07-21 15:45:41 -0400

    Remove old display helpers
    This logic will live elsewhere, not here. It should be in the front end at the very end. Data should be passable until then.

--- FocusFlow/Helpers/TaskDisplayHelper.cs
+++ FocusFlow/Helpers/TaskDisplayHelper.cs
@@ -7,19 +7,6 @@ public static class TaskDisplayHelper
 {
     private const uint SecondsPerMinute = 60; 
     private const uint SecondsPerHour = 60 * 60;
-    
-    public static string FormatSecondsLogged(long seconds)
-    {
-        var hours = seconds / SecondsPerHour;
-        var minutes = (seconds % SecondsPerHour) / SecondsPerMinute;
-        
-        return  $"{hours:D2}h {minutes:D2}m";
-    }
-
-    public static string FormatDateOnly(DateOnly date)
-    {
-        return date.ToString("ddd, MMM dd");
-    }
 
     public static TaskSummaryViewModel TaskEntityToView(Task task)
     {
