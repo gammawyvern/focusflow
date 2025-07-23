f1b9be6166d04550ae61bf803c9d394d7aa4c690
Keagen Talsma <keegenjt@gmail.com>
2025-07-23 19:41:23 -0400

    Create simple task table component

---
+++ FocusFlow/client-ui/js/components/Task/TaskTable.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import TaskRow from './TaskRow';
+
+const TaskTable = ({ tasks }) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Status</th>
+                    <th>Information</th>
+                    <th>Due Date</th>
+                    <th>Time Logged</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tasks.map(task => (
+                    <TaskRow key={task.id} task={task} />
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default TaskTable;
