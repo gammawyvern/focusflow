5167fafc946fa3b2728db619331625aacba0004d
Keagen Talsma <keegenjt@gmail.com>
2025-07-23 17:08:55 -0400

    Update API with needed CRUD calls

--- FocusFlow/Controllers/Api/TaskApiController.cs
+++ FocusFlow/Controllers/Api/TaskApiController.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 
 using FocusFlow.Helpers.Mapping;
-using FocusFlow.Data.Entities;
 using FocusFlow.Repositories;
+using FocusFlow.Dtos;
 
 namespace FocusFlow.Controllers.Api;
 
@@ -10,14 +10,56 @@ namespace FocusFlow.Controllers.Api;
 [Route("api/tasks")]
 public class TaskApiController(ITaskItemRepository taskItemRepository) : Controller
 {
+    [HttpPost]
+    public async Task<IActionResult> CreateTask([FromBody] TaskItemDto dto)
+    {
+        var entity = TaskItemMapper.ToEntity(dto);
+        await taskItemRepository.AddAsync(entity);
+        await taskItemRepository.SaveChangesAsync();
+        
+        var resultDto = TaskItemMapper.ToDto(entity);
+
+        return CreatedAtAction(nameof(GetTask), new { id = resultDto.Id }, resultDto);
+    }
+    
     [HttpGet]
     public async Task<IActionResult> GetTasks()
     {
-        var tasks = await taskItemRepository.GetAllAsync();
-        var taskViews = tasks
-            .Select(TaskItemMapper.ToDto)
-            .ToList();
+        var entities = await taskItemRepository.GetAllAsync();
+        var dtos = entities.Select(TaskItemMapper.ToDto).ToList();
+        return Ok(dtos);
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetTask([FromRoute] int id)
+    {
+        var entity = await taskItemRepository.GetByIdAsync(id);
+        return entity == null ? NotFound() : Ok(TaskItemMapper.ToDto(entity));
+    }
+    
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> UpdateTask([FromRoute] int id, [FromBody] TaskItemDto dto)
+    {
+        if (id != dto.Id) { return BadRequest("Mismatched task IDs."); }
         
-        return Ok(taskViews);
+        var entity = await taskItemRepository.GetByIdAsync(dto.Id);
+        if (entity == null) { return NotFound(); }
+        
+        TaskItemMapper.ApplyDtoToEntity(dto, entity);
+        await taskItemRepository.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteTask([FromRoute] int id)
+    {
+        var entity = await taskItemRepository.GetByIdAsync(id);
+        if (entity == null) { return NotFound(); }
+        
+        taskItemRepository.Delete(entity);
+        await taskItemRepository.SaveChangesAsync();
+        
+        return NoContent();
     }
 }
