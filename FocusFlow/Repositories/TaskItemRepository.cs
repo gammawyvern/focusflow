52cea62c13f3378dd6ef3b844b170d4c324b5b0e
Keagen Talsma <keegenjt@gmail.com>
2025-07-23 17:06:27 -0400

    Add and fix necessary Repository functions (SaveCHanges)

--- FocusFlow/Repositories/TaskItemRepository.cs
+++ FocusFlow/Repositories/TaskItemRepository.cs
@@ -1,6 +1,7 @@
-using FocusFlow.Data;
-using FocusFlow.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+
+using FocusFlow.Data.Entities;
+using FocusFlow.Data;
 
 namespace FocusFlow.Repositories;
 
@@ -8,6 +9,8 @@ public class TaskItemRepository(AppDbContext appDbContext): ITaskItemRepository
 {
     private readonly AppDbContext _appDbContext = appDbContext;
     
+    /* IRepository Functions. */
+
     public async Task<TaskItem?> GetByIdAsync(int id)
     {
         return await _appDbContext.TaskItems.FindAsync(id);
@@ -23,12 +26,27 @@ public class TaskItemRepository(AppDbContext appDbContext): ITaskItemRepository
         _appDbContext.TaskItems.Add(entity);
     }
 
+    public async Task AddAsync(TaskItem entity)
+    {
+        await _appDbContext.TaskItems.AddAsync(entity);
+    }
+
     public void Delete(TaskItem entity)
     {
         _appDbContext.TaskItems.Remove(entity);
     }
-    
-    /* Custom Task Functions. */
+
+    public void SaveChanges()
+    {
+        _appDbContext.SaveChanges();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _appDbContext.SaveChangesAsync();
+    }
+
+    /* ITaskRepository Functions. */
 
     public IEnumerable<TaskItem> GetTaskItemsByDueDate(DateOnly dueDate)
     {
