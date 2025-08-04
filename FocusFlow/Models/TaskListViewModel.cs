ccdd34ab7c9b2f825b4fecaad929d42a17a11384
Keagen Talsma <keegenjt@gmail.com>
2025-08-03 23:39:26 -0400

    Put tasks into sublists

--- FocusFlow/Models/TaskListViewModel.cs
+++ FocusFlow/Models/TaskListViewModel.cs
@@ -3,9 +3,11 @@ namespace FocusFlow.Models;
 public class TaskListViewModel
 {
     public string? Header { get; init; }
-    public IEnumerable<TaskItemViewModel> TaskItems { get; init; } = [];
     
-    public bool ShowCompleted { get; init; } = true;
+    public IEnumerable<TaskItemViewModel> ActiveTaskItems { get; init; } = [];
+    public IEnumerable<TaskItemViewModel> IncompleteTaskItems { get; init; } = [];
+    public IEnumerable<TaskItemViewModel> CompleteTaskItems { get; init; } = [];
+    
     public bool ShowDateSelector { get; init; } = true;
     
     public DateTime? StartDate { get; init; }
--- FocusFlow/ViewComponents/TaskListViewComponent.cs
+++ FocusFlow/ViewComponents/TaskListViewComponent.cs
@@ -8,41 +8,54 @@ namespace FocusFlow.ViewComponents;
 
 public class TaskListViewComponent(ITaskItemRepository taskItemRepository): ViewComponent
 {
-    public async Task<IViewComponentResult> InvokeAsync(string? header, bool? showCompleted, bool? showDateSelector, DateTime? startDate, DateTime? endDate)
+    public async Task<IViewComponentResult> InvokeAsync(string? header, bool? showDateSelector, DateTime? startDate, DateTime? endDate)
     {
         var entities = await taskItemRepository.GetAllAsync();
         
-        entities = entities
-            .Where(e => showCompleted == true || showCompleted == null || !e.IsCompleted)
-            .OrderBy(e => e.IsCompleted)
-            .ThenByDescending(e => e.IsActive)
-            .ThenBy(e => e.DueDate.Date)
-            .ToList();
-
         if (startDate.HasValue)
         {
-            entities = entities
-                .Where(task => task.DueDate.Date >= startDate.Value.Date)
-                .ToList();
+            entities = entities.Where(task => task.DueDate.Date >= startDate.Value.Date).ToList();
         }
 
         if (endDate.HasValue)
         {
-            entities = entities
-                .Where(t => t.DueDate.Date <= endDate.Value.Date)
-                .ToList();
+            entities = entities.Where(t => t.DueDate.Date <= endDate.Value.Date).ToList();
         }
-
-        var taskItemViewModels = entities
+        
+        // Convert to DTOs
+        
+        var taskItemDtos = entities
             .Select(TaskItemMapper.ToDto)
+            .ToList();
+        
+        // Map DTOs to needed lists
+
+        var activeTasks= taskItemDtos
+            .Where(task => task.IsActive)
+            .OrderBy(task => task.DueDate)
+            .Select(TaskItemViewModelMapper.ToTaskItemViewModel)
+            .ToList();
+
+        var incompleteTasks = taskItemDtos
+            .Where(task => !task.IsCompleted && !task.IsActive)
+            .OrderBy(task => task.DueDate)
+            .Select(TaskItemViewModelMapper.ToTaskItemViewModel)
+            .ToList();
+
+        var completedTasks = taskItemDtos
+            .Where(task =>  task.IsCompleted && !task.IsActive)
+            .OrderBy(task => task.DueDate)
             .Select(TaskItemViewModelMapper.ToTaskItemViewModel)
             .ToList();
 
         var model = new TaskListViewModel
         {
             Header = header,
-            TaskItems = taskItemViewModels,
-            ShowCompleted = showCompleted ?? true,
+            
+            ActiveTaskItems = activeTasks,
+            IncompleteTaskItems = incompleteTasks,
+            CompleteTaskItems = completedTasks,
+            
             ShowDateSelector = showDateSelector ?? true,
             StartDate = startDate,
             EndDate = endDate
